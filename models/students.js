13f3647e43826881342937ace4cbf55b9b8ed61b
TegarAlfaR <tegaralfa18052004@gmail.com>
2024-10-17 23:38:52 +0700

    sequelized validation

---
+++ models/students.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Students extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Students.init({
+    last_name: DataTypes.STRING,
+    first_name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: "Email already exist"
+      },
+      validate: {
+        isEmail: {
+          msg: "Please enter vaild email"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate:{
+        len: {
+          args: [6, 10],
+          msg: "Minimal parrword lenght is 6"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Students',
+  });
+  return Students;
+};
